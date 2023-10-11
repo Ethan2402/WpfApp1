627461c938ed2584d4fc79f5c32f6d3c39fad44a
22000917ctc <22000917ctc@MA-CTC-SDD-S07>
2023-10-11 12:38:20 -0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐢","🐢",
+                "🦔","🦔",
+                "🐿","🐿",
+                "🦈","🦈",
+                "🦀","🦀",
+                "🐄","🐄",
+                "🦚","🦚",
+                "🐌","🐌",
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in maingrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextemoji = animalEmoji[index];
+                textBlock.Text = nextemoji;
+                animalEmoji.RemoveAt(index);
+
+            }
+        }
+    }
+}
